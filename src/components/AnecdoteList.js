e9b8c42bfc3a9b8a7e92528fa8fe3a2ed3bf2490
Tomi <tomi.taskila@tyomaa.com>
2021-08-10 18:15:12 +0300

    6.11 Better anecdotes, step9

--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addVote } from '../reducers/anecdoteReducer'
+import { setNotification, removeNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
   const anecdotes = useSelector(state => state.anecdotes)
   const anecdotesSorted = [...anecdotes].sort((a, b) => (a.votes > b.votes) ? -1 : 1)
   const dispatch = useDispatch()
+
+  const handleVote = (anecdote) => {
+    dispatch(addVote(anecdote.id))
+    dispatch(setNotification(`You voted '${anecdote.content}'`))
+    setTimeout(() => {
+      dispatch(removeNotification())
+    }, 5000)
+  }
 
   return (
     <>
@@ -16,7 +25,7 @@ const AnecdoteList = () => {
           </div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => dispatch(addVote((anecdote.id)))}>vote</button>
+            <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
       )}
--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -1,7 +1,25 @@
-const initialNotification = 'Hello world'
+const reducer = (state = null, { type, content }) => {
+  switch(type) {
+    case 'SET_NOTIFICATION':
+      return content
+    case 'REMOVE_NOTIFICATION':
+      return null
+    default:
+      return state
+  }
+}
 
-const reducer = (state = initialNotification, action) => {
-  return state
+export const setNotification = (content) => {
+  return {
+    type: 'SET_NOTIFICATION',
+    content,
+  }
+}
+
+export const removeNotification = () => {
+  return {
+    type: 'REMOVE_NOTIFICATION',
+  }
 }
 
 export default reducer
