7885626a703e17e499bdaed33fe09b6128677e47
Tomi <tomi.taskila@tyomaa.com>
2021-08-11 10:50:39 +0300

    6.16 Anecdotes and the backend, step4

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -27,10 +27,13 @@ export const addVote = (id) => {
   }
 }
 
-export const createAnecdote = (data) => {
-  return {
-    type: 'NEW_ANECDOTE',
-    data,
+export const createAnecdote = (content) => {
+  return async (dispatch) => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch({
+      type: 'NEW_ANECDOTE',
+      data: newAnecdote,
+    })
   }
 }
 
